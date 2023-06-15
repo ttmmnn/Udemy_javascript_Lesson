68a8ced83ab7dd075212fab95ed0cc6328997b7f
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-06-15 17:56:12 +0900

    add_practice

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 let count = 0;
 count = 30;
 let newCount = 0;
+const daysInWeek = 7;
+let tomatoCount;
+let tomato$Count;
+let tomato_Count;
+let トマトカウント;
 
-// 次は定数から
+let additionalResult = 2 + 5;
+let result = 5;
+result = result + 1;
+result += 1;
+result++;
+++result;
+let number = 10;
+number = -4;
+number = 32.42;
+let string = 'Hello';
+number = 'something';
+const userName = 'yoshipi';
+string = 'Hello ' + userName + '!';
+string = `Hello ${userName}!`;
+string = 'Hello\nyoshipi!';
+string = `Hello
+yoshipi!`;
+const userInput = '10.9';
+let calcResult;
+calcResult = Number(userInput) + 10;
+calcResult = parseInt(userInput) + 10;
+calcResult = parseFloat(userInput) + 10;
+calcResult = +userInput + 10;
+
+const tenNumber = 10;
+calcResult = '10' + String(tenNumber);
+calcResult = '10' + tenNumber.toString();
+console.log(calcResult);
